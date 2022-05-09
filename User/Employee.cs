89c8956ae9a5b9a38164d2714a963ded70844cb6
LarsHartendorp <690720@student.inholland.nl>
2022-05-09 09:56:12 +0200

    Employee class

--- User/Employee.cs
+++ User/Employee.cs
@@ -10,6 +10,11 @@ namespace ChapeauModel
         // nog helemaal aanpassen. moet alles erin wat ook in sql staat? email, phonenumber etc... 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        
+        public int Category { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Questiom { get; set; }
+        public string Answer { get; set; }
     }
 }
